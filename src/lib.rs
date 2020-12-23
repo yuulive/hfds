bb3531b563de21db5b2302ead007c7abd08be8ca
michael <mcarson898@gmail.com>
2020-12-23 00:38:08 -0600

    Added README and usage docs
    This should make using this library easier. This also added tests that
    should help prevent bugs/issues.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,53 @@
+//! # Syncwrap
+//!
+//! Wraps asynchronous functions in order to make them synchronous based on if a
+//! "sync" feature is enabled. This is useful when writting http clients as you
+//! can write async methods and wrap them for use in synchronous code bases
+//! automatically.
+//!
+//! # Usage
+//! ```toml
+//! [dependencies]
+//! syncwrap = "0.1.0"
+//! ```
+//!
+//! # Examples
+//!
+//! ```rust
+//! #[syncwrap::wrap]
+//! async fn foo(input: &str) -> String {
+//!   format!("I am {} now", input)
+//! }
+//!
+//! fn main() {
+//!   let out = foo("sync");
+//!   assert_eq!(out, "I am sync now".to_owned())
+//! }
+//! ```
+//!
+//! Currently the wrapping is very naive and simply wraps the function in
+//! tokio::main. This is likey more expensive then it needs to be and I hope
+//! to make it more efficient later.
+
 use syn;
 use quote::quote;
 use proc_macro::TokenStream;
 
-
+/// Wraps an async function in order to make it synchronous
+///
+/// # Examples
+///
+/// ```
+/// #[syncwrap::wrap]
+/// async fn foo(input: &str) -> String {
+///  format!("I am {} now", input)
+/// }
+///
+/// let out = foo("sync");
+/// assert_eq!(out, "I am sync now".to_owned())
+/// ```
 #[proc_macro_attribute]
-pub fn wrapper(_meta: TokenStream, input: TokenStream) -> TokenStream {
+pub fn wrap(_meta: TokenStream, input: TokenStream) -> TokenStream {
   // parse the input stream into our async function
   let func = syn::parse_macro_input!(input as syn::ItemFn);
   // get visibility of function
